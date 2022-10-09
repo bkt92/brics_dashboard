d83ea844017cec5d9789b00c2d918ca04e1b47b6
bkt92 <bkt92@yahoo.com.vn>
2022-10-09 21:39:07 +0700

    update

--- Overview.py
+++ Overview.py
@@ -16,11 +16,15 @@ def data_download(tickers, period):
     yf_data_hist = yf_data.history(period=period, interval = "1d");
     return pd.DataFrame(yf_data_hist["Close"])
     
-# Download data
-data = data_download('BRL=X,RUB=X,INR=X,CNY=X,ZAR=X', '12y')
+# Initialization data
+if 'data' not in st.session_state:
+    st.session_state['data'] = data_download('BRL=X,RUB=X,INR=X,CNY=X,ZAR=X', '12y')
+    
+# Get data
+data = st.session_state['data']
     
 current_price = data_download('BRL=X,RUB=X,INR=X,CNY=X,ZAR=X', '3d').dropna()
-st.markdown("### Current rate and prediction (up/down compare to current price):")
+st.markdown("#### Current rate and prediction (up/down compare to current price):")
 col1_0, col2_0, col3_0, col4_0, col5_0 = st.columns(5)
 col1_0.metric("BRL", np.round(current_price[['BRL=X']].iloc[-1].to_numpy(),2), "1%")
 col2_0.metric("RUB", np.round(current_price[['RUB=X']].iloc[-1].to_numpy(),2), "-8%")
@@ -28,7 +32,7 @@ col3_0.metric("INR", np.round(current_price[['INR=X']].iloc[-1].to_numpy(),2), "
 col4_0.metric("CNY", np.round(current_price[['CNY=X']].iloc[-1].to_numpy(),2), "4%")
 col5_0.metric("ZAR", np.round(current_price[['ZAR=X']].iloc[-1].to_numpy(),2), "4%")
 
-st.markdown("### 1 USD exchange rate plot:")
+st.markdown("#### 1 USD exchange rate plot:")
  
 # Plot the data series
 col1_1, col2_1 = st.columns(2)
@@ -38,13 +42,12 @@ with col1_1:
     'Choose currency to plot:',
     ['BRL', 'RUB', 'INR', 'CNY', 'ZAR'],
     ['BRL', 'RUB', 'INR', 'CNY', 'ZAR'])
-tickers = ''
 options = [option + '=X' for option in options]
     
 with col2_1:
     d1 = st.date_input(
     "Start date:",
-    datetime.date.today()-relativedelta(months=+12))
+    datetime.date.today()-relativedelta(months=+120))
     d2 = st.date_input(
     "End date:",
     datetime.date.today())
@@ -53,28 +56,39 @@ with col2_1:
 st.line_chart(data[options].loc[d1:d2, :])
 
 # Calculate return series 
-
 data_returns = data.copy()
 data_returns= data_returns.pct_change()
 data_returns.replace([np.inf, - np.inf], np.nan, inplace = True)
 data_returns = data_returns.dropna()
+# Initialization data
+if 'data_returns' not in st.session_state:
+    st.session_state['data_returns'] = data_returns
 
 # Cumulated return
+st.markdown("#### Cumulated return:")
 st.line_chart(data_returns.add(1).cumprod().loc[d1:d2, :])
 
 # Correlation plot
 col1_2, col2_2 = st.columns(2)
 
-Ind_cov = data_returns.corr()
+Ind_corr = data_returns.corr()
+plt.figure(figsize=(7,7))
+sns.color_palette("coolwarm", as_cmap=True)
+mask = np.triu(np.ones_like(Ind_corr.corr()))
+fig, ax = plt.subplots()
+ax = sns.heatmap(Ind_corr, annot = True, center=0, cmap="coolwarm", mask=mask)
+
+
+Ind_cov = data_returns.cov()
 plt.figure(figsize=(7,7))
 sns.color_palette("coolwarm", as_cmap=True)
 mask = np.triu(np.ones_like(Ind_cov.corr()))
-fig, ax = plt.subplots()
-ax = sns.heatmap(Ind_cov, annot = True, center=0, cmap="coolwarm", mask=mask)
+fig1, ax1 = plt.subplots()
+ax1 = sns.heatmap(Ind_cov, annot = True, center=0, cmap="coolwarm", mask=mask)
 
 with col1_2:
     st.markdown("#### Covariance matrix")
-    st.pyplot(fig)
+    st.pyplot(fig1)
 
 with col2_2:
     st.markdown("#### Correlation matrix")
--- pages/1_Brazil.py
+++ pages/1_Brazil.py
@@ -1,11 +1,60 @@
 import streamlit as st
 import numpy as np
 import datetime
+from dateutil.relativedelta import relativedelta
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.markdown("# Brazil")
 st.write("This page provied detail statistical analysis about Brazil forex market")
 
-d = st.date_input(
-    "When's your birthday",
-    datetime.date(2019, 7, 6))
-st.write('Your birthday is:', d)
+# Plot the data series
+col1_1, col2_1 = st.columns(2)
+
+with col1_1:
+    options = st.multiselect(
+    'Indicates to display:',
+    ['SMA100', 'EMA100', 'SMA20', 'EMA20'],
+    ['SMA100'])
+    
+with col2_1:
+    d1 = st.date_input(
+    "Start date:",
+    datetime.date.today()-relativedelta(months=+120))
+    d2 = st.date_input(
+    "End date:",
+    datetime.date.today())
+    
+data = st.session_state['data']
+data_returns = st.session_state['data_returns']
+
+i100day_SMA = data[['BRL=X']].rolling(window=100).mean()
+i100day_EMA = data[['BRL=X']].ewm(span=100, adjust=False).mean()
+i20day_SMA = data[['BRL=X']].rolling(window=20).mean()
+i20day_EMA = data[['BRL=X']].ewm(span=20, adjust=False).mean()
+
+fig, ax = plt.subplots()
+#figsize=(10,10)
+ax.plot(data.loc[d1:d2, :].index, data.loc[d1:d2, 'BRL=X'], label='Idx')
+if "SMA100" in options:
+    ax.plot(i100day_SMA.loc[d1:d2, :].index, i100day_SMA.loc[d1:d2, 'BRL=X'], 
+        label = '100-days SMA',linestyle= '--') 
+if "EMA100" in options:
+    ax.plot(i100day_EMA.loc[d1:d2, :].index, i100day_EMA.loc[d1:d2, 'BRL=X'], 
+        label = '100-days SMA',linestyle= '--')         
+if "SMA20" in options:
+    ax.plot(i20day_SMA.loc[d1:d2, :].index, i20day_SMA.loc[d1:d2, 'BRL=X'], 
+        label = '20-days SMA',linestyle= '--')
+if "EMA20" in options:
+    ax.plot(i20day_EMA.loc[d1:d2, :].index, i20day_EMA.loc[d1:d2, 'BRL=X'], 
+        label = '20-days EMA',linestyle= ':', color = 'k')
+
+ax.legend(loc='best')
+ax.set_ylabel('BRL')
+
+st.pyplot(fig)
+
+fig1, ax1 = plt.subplots()
+ax1 = sns.histplot(data = data_returns[['BRL=X']], kde = True)
+#ax1.title('BRL Return')
+st.pyplot(fig1)
