0ba34c7beb109862eb6df2b3e4bd47e78db79aef
bkt92 <bkt92@yahoo.com.vn>
2022-10-09 09:45:08 +0700

    init dashboard

---
+++ pages/3_india.py
@@ -0,0 +1,4 @@
+import streamlit as st
+import numpy as np
+
+st.write("This page provied detail statistical analysis about India forex market")
---
+++ pages/About.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import numpy as np
+
+st.write("This dashboard have made as part of Group 02 capstone project")
+st.write("MScFE 690 Capstone (C21-S1) - Worldquant University")
+st.write("Contributing members:")
+st.write("Bui Khac Tu - bkt992@gmail.com")
+st.write("Avinash Thavva - thavvaavinash@gmail.com")
+st.write("Saransh Agrawal - isaranshagrawal@gmail.com")
